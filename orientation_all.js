9ccd29cd1c94b662380086be52a39029b82fa43d
Bordea Claudiu <123594015+Cloudiu9@users.noreply.github.com>
2023-11-12 15:46:14 +0200

    Update orientation_all.js

--- orientation_all.js
+++ orientation_all.js
@@ -1,10 +1,5 @@
 window.addEventListener("deviceorientation", on_device_orientation);
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var centru = { x: canvas.width / 2, y: canvas.height / 2 };
-var latura_patrat = 50;
-
 function on_device_orientation(evt) {
     var alpha = evt.alpha;
     var beta = evt.beta;
@@ -14,25 +9,34 @@ function on_device_orientation(evt) {
     document.getElementById("b").innerHTML = "beta = " + beta;
     document.getElementById("c").innerHTML = "gamma = " + gamma;
 
-    // Clear canvas and draw based on each angle
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var centru = { x: canvas.width / 2, y: canvas.height / 2 };
+    var latura_patrat = 50;
+
     context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#6A0888";
+    context.save();
+    context.translate(centru.x, centru.y);
 
     // Draw based on alpha angle
-    context.save();
-    context.translate(centru.x / 2, centru.y / 2);
-    context.rotate(alpha * Math.PI / 180);
+    context.rotate(alpha * (Math.PI / 180));
     context.beginPath();
     context.rect(-latura_patrat / 2, -latura_patrat / 2, latura_patrat, latura_patrat);
     context.stroke();
-    context.restore();
 
     // Draw based on beta angle
-    context.fillStyle = "#6A0888";
+    context.rotate(-alpha * (Math.PI / 180)); // Undo alpha rotation
+    context.rotate(beta * (Math.PI / 180));
     context.beginPath();
-    context.fillRect(180, 150, beta, 50);
+    context.fillRect(-latura_patrat / 2, -latura_patrat / 2, latura_patrat, latura_patrat);
 
     // Draw based on gamma angle
-    context.fillStyle = "#6A0888";
+    context.rotate(-beta * (Math.PI / 180)); // Undo beta rotation
+    context.rotate(gamma * (Math.PI / 180));
     context.beginPath();
-    context.fillRect(180, 150, 50, gamma);
+    context.arc(0, 0, latura_patrat / 2, 0, 2 * Math.PI);
+    context.stroke();
+
+    context.restore(); // Restore the saved state
 }
